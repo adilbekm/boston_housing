a1b53757f41ffdba7e108b268d6981c5c2181e94
adilbekm <adilbekm@yahoo.com>
2017-12-03 13:54:52 -0500

    update answer to question 10 per udacity

--- explore.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-from sklearn.model_selection import ShuffleSplit
-
-data = pd.read_csv('housing.csv')
-prices = data['MEDV']
-features = data.drop('MEDV', axis = 1)
